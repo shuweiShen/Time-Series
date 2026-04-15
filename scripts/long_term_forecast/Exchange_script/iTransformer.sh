7f94a77b7426f9afa8265103a34ada7b9666e7bc
shuweiShen <95003723+shuweiShen@users.noreply.github.com>
2026-04-15 10:25:57 +0800

    Add files via upload

---
+++ scripts/long_term_forecast/Exchange_script/iTransformer.sh
@@ -0,0 +1,32 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=iTransformer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id ECL_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 192 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 3 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.0005 \
+  --itr 1 \
+  --shift 7 \
+  --beta 0.5 \
+  --gamma 0.5
+
---
+++ scripts/long_term_forecast/Weather_script/PatchTST.sh
@@ -0,0 +1,106 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=PatchTST
+
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --itr 1 \
+  --n_heads 4 \
+  --batch_size 128 \
+  --beta 0.1 \
+  --gamma 0.1 \
+  --shift 8
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --itr 1 \
+  --n_heads 4 \
+  --batch_size 128 \
+  --beta 0.2 \
+  --gamma 0.2 \
+  --shift 7
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --itr 1 \
+  --n_heads 4 \
+  --batch_size 128 \
+  --shift 7
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --itr 1 \
+  --n_heads 4 \
+  --batch_size 128 \
+
+  --shift 6
